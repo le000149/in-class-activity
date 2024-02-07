2cac2599f08f4ad4169624296a2569aa46581e3e
yvonn <le000149@algonquinlive.com>
2024-02-07 16:17:09 -0500

    Task 3

--- app/src/main/java/com/college/converter/MainActivity.java
+++ app/src/main/java/com/college/converter/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.util.Log;
 /*
     TODOs:
     In groups of 4, complete the following tasks, 1 for each team member:
@@ -24,32 +24,37 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     static private final Float CONVERSION_RATE = 0.80F;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Log entry message for onCreate() method
+        Log.i("MainActivity", "onCreate() method called");
+
         Button buttonConvert = findViewById(R.id.convertButton);
 
-        buttonConvert.setOnClickListener( view ->  {
+        buttonConvert.setOnClickListener(view -> {
             convertCurrency(view);
-        } );
+        });
     }
 
     public void convertCurrency(View view) {
+        // Log entry message for convertCurrency() method
+        Log.i("MainActivity", "convertCurrency() method called");
 
         EditText inputView = findViewById(R.id.entryId);
-
         String inputAmount = inputView.getText().toString();
-
         TextView resultView = findViewById(R.id.resultId);
 
         if (!inputAmount.isEmpty()) {
             Float inputAmountDecimal = Float.valueOf(inputAmount);
-
             Float resultFloat = inputAmountDecimal * CONVERSION_RATE;
-
-            resultView.setText( resultFloat + " Euros" );
+            resultView.setText(resultFloat + " Euros");
         }
+
+        // Log exit message for convertCurrency() method
+        Log.i("MainActivity", "convertCurrency() method finished");
     }
 }
